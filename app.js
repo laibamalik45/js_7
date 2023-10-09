4de15bdde0e590e1441bfcfdc574c74c22a16cc3
laibamalik45 <laibamalik45d@gamil.com>
2023-10-09 17:10:41 +0500

    now ready to commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// function playSound(){
+//     var audio = new Audio ('interface-124464.mp3');
+//     audio.play();
+// }
+
+// function get(){
+//     var a = document.getElementById("userinput");
+//     console.log(a.value);
+//     userinput.value=" ";
+// // }
+// function get(){
+//     var a = document.getElementById("value1");
+//     console.log(a.value)
+// }
+
+// function get(){
+//     var a = document.getElementById("userinput");
+//     console.log(a.value);
+// }
+// function get(){
+//     var a = document.getElementById("enter");
+//     console.log(a.value); 
+// }
+
+function greet(){
+    var a = document.getElementById("value1")
+    var a = document.getElementById("value2")
+    var a = document.getElementById("value3")
+    var a = document.getElementById("value4")
+    var a = document.getElementById("value5")
+    var a = document.getElementById("value6")
+    // var a = document.getElementById("value7")
+    console.log(value1.value,value2.value,value3.value,value4.value,value5.value,value6.value);
+
+
+
+}
